73a2647f7d2f193ded6508700df43b17b0e7737f
ryosuke-horie <ryosuke03140@gmail.com>
2024-02-22 21:45:22 +0900

    Inertia&Breezeインストール

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        host: "0.0.0.0",
+        hmr: {
+            host: "localhost",
+        },
+        watch: {
+            usePolling: true,
+        },
+        port: 5173,
+    },
 });
