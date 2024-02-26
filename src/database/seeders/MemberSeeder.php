0ff019be940774fb7a5c95c65a4c59c3b75c591f
ryosuke-horie <ryosuke03140@gmail.com>
2024-02-26 19:12:06 +0900

    Seedingを登録

--- src/database/seeders/MemberSeeder.php
+++ src/database/seeders/MemberSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Member;
 
 class MemberSeeder extends Seeder
 {
@@ -11,6 +12,23 @@ class MemberSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // ユーザーを作成
+        Member::create([
+            'user_id' => 1,
+            'name' => 'テスト太郎',
+            'nickname' => 'テストニックネーム',
+        ]);
+
+        Member::create([
+            'user_id' => 1,
+            'name' => 'テスト次郎',
+            'nickname' => 'テストニックネーム2',
+        ]);
+
+        Member::create([
+            'user_id' => 1,
+            'name' => 'テスト三郎',
+            'nickname' => 'テストニックネーム3',
+        ]);
     }
 }
